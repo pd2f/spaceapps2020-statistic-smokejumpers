a3f1f23c2948c1f4e8425e5f6c1727b183cecb1f
pd2f <paulodanielff@gmail.com>
2020-10-03 14:37:19 -0300

    cadastratro de ativo

--- Ativo.py
+++ Ativo.py
@@ -3,6 +3,7 @@ import os
 from pymongo import MongoClient
 import dns
 from bson.objectid import ObjectId
+import json
 
 scon = "mongodb+srv://admin:adminadmin@cluster0.5oigr.gcp.mongodb.net/nasa-space-apps-challenge?w=majority"
 
@@ -11,8 +12,9 @@ def cadastrar_ou_retornar_ativo(id,gjson):
     base_estatistica = client.statistical_skope
     resultado = base_estatistica.ativos.find({"identificador":id}).sort("_id",-1).limit(1)
     if resultado.count() == 0:
+        gjson["identificador"]=  id
         unico = base_estatistica.ativos.insert_one(gjson)
-        retorno = (200,"Ativo "+id+"("+str(unico)+") cadastrado com sucesso!")
+        retorno = (200,"Ativo "+str(id)+" cadastrado com sucesso!")
     else:
         retorno = (409,"Ativo anteriormente cadastrado.")
     client.close()
