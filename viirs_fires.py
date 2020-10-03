41f2bee59565ae2cc3cbd5e723d5258b796c24f0
pd2f <paulodanielff@gmail.com>
2020-10-03 19:13:52 -0300

    ajuste mock

--- viirs_fires.py
+++ viirs_fires.py
@@ -1,5 +1,5 @@
 import pandas as pd
-url_mock = "C:\\Users\\Usuario\\Documents\\projetos\\software\\spaceapps2020\\mock\\mock_20200505.pickle"
+url_mock = "./mock/mock_20200505.pickle"
 #TODO criar serviços para obter dados do viirs
 df = pd.read_pickle(url_mock)
 def get_VIIRS_data():
