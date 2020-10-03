8a7d87fa586cc1d11ba70dc426f5cddd55d57e57
pd2f <paulodanielff@gmail.com>
2020-10-03 13:46:40 -0300

    operação de cadastro

--- Ativo.py
+++ Ativo.py
@@ -1,7 +1,22 @@
 import geojson
 import os
-def cadastrar_ativo(id,gjson):
-    return "Ativo cadastrado com sucesso!"
+from pymongo import MongoClient
+import dns
+from bson.objectid import ObjectId
+
+scon = "mongodb+srv://admin:adminadmin@cluster0.5oigr.gcp.mongodb.net/nasa-space-apps-challenge?w=majority"
+
+def cadastrar_ou_retornar_ativo(id,gjson):
+    client = MongoClient(scon)
+    base_estatistica = client.statistical_skope
+    resultado = base_estatistica.ativos.find({"identificador":id}).sort("_id",-1).limit(1)
+    if resultado.count() == 0:
+        unico = base_estatistica.ativos.insert_one(gjson)
+        retorno = (200,"Ativo "+id+"("+str(unico)+") cadastrado com sucesso!")
+    else:
+        retorno = (409,"Ativo anteriormente cadastrado.")
+    client.close()
+    return retorno
 
 def inferencia(id):
     if id ==1 :
--- main.py
+++ main.py
@@ -49,8 +49,10 @@ class cadastro_ativo(Resource):
     def post(self,id):
         #json_data = request.get_json(force=True)
         json_data="teste_mock"
-        ret = atv.cadastrar_ativo(id,json_data)
-        return jsonify(mensagem=ret)
+        ret = atv.cadastrar_ou_retornar_ativo(id,json_data)
+        if ret[0] != 200:
+            abort(ret[0], description=ret[1])
+        return ret[1],ret[0]
 
     def get(self,id):
         return  atv.inferencia(id)
